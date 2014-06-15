b3371c85712d0747e5a8f5f1850f1b53fdea742d
Onur EKER <onur.eker@gmail.com>
2014-06-15 18:26:51 +0300

    * ParameterProvider enum cast is added.

---
+++ Source/Projects/Bosphorus.Configuration.Core/Parameter.Provider/ParameterProviderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bosphorus.Configuration.Core.Parameter.Provider
+{
+    public static class ParameterProviderExtensions
+    {
+        public static TEnum GetValueAsEnum<TEnum>(this IParameterProvider extended, string parameterName)
+        {
+            string parameterValue = extended.GetValue(parameterName);
+            Type enumType = typeof (TEnum);
+            TEnum result = (TEnum) Enum.Parse(enumType, parameterValue);
+            return result;
+        }
+    }
+}
